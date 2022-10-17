2df03d5a466a14173eb2398230781b22940c1751
berryberry189 <berryberry189@naver.com>
2022-10-17 20:43:43 +0900

    Initial commit

---
+++ src/test/java/grace/datastructuresgroupstudy/DataStructuresGroupStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package grace.datastructuresgroupstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataStructuresGroupStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
