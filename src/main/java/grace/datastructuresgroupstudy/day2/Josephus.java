3b9f5820416b52d1ea18e7b38d128d3b85c2142a
berryberry189 <berryberry189@naver.com>
2022-10-23 17:54:48 +0900

    Josephus Problem

---
+++ src/main/java/grace/datastructuresgroupstudy/day2/Josephus.java
@@ -0,0 +1,33 @@
+package grace.datastructuresgroupstudy.day2;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class Josephus {
+
+  public static void main(String[] args) {
+    int n = 9;
+    int k = 3;
+
+    List<Integer> resultList = new ArrayList<>();
+    Queue<Integer> queue = new LinkedList<Integer>();
+
+    for(int i=1; i<=n; i++) {
+      queue.add(i);
+    }
+
+    int cnt = 0;
+    while(!queue.isEmpty()) {
+      cnt++;
+      int temp = queue.poll();
+      if(cnt % k == 0) resultList.add(temp);
+      else queue.add(temp);
+    }
+
+    System.out.println(resultList.toString());
+
+  }
+
+}
