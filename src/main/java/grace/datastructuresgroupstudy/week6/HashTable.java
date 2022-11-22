f23b4c5634129c1b9e3772f5d6dc5cda06d94c3a
berryberry189 <berryberry189@naver.com>
2022-11-22 22:59:02 +0900

    간단한 HashTable 구현

---
+++ src/main/java/grace/datastructuresgroupstudy/week6/HashTable.java
@@ -0,0 +1,69 @@
+package grace.datastructuresgroupstudy.week6;
+
+import java.util.LinkedList;
+
+public class HashTable {
+
+  private LinkedList<Node>[] table;
+
+  public HashTable(int size) {
+    table = new LinkedList[size];
+  }
+
+  Long getHashCode(String key) {
+    Long hashCode = 0L;
+
+    for (char c : key.toCharArray()) {
+      hashCode += (long) c;
+    }
+
+    return hashCode;
+  }
+
+  public int getIndex(Long hashCode) {
+    return (int) (hashCode % table.length);
+  }
+
+  Node searchNode(int index, String key) {
+    LinkedList<Node> indexedList = table[index];
+
+    for (Node n : indexedList) {
+      if (n.key == key) {
+        return n;
+      }
+    }
+    return null;
+  }
+
+  public void put(String key, String value) {
+    Long hashCode = getHashCode(key);
+    int index = getIndex(hashCode);
+
+    if (table[index] == null) {
+      table[index] = new LinkedList<Node>();
+      table[index].add(new Node(key, value));
+    } else {
+      Node searched = searchNode(index, key);
+
+      if (searched != null) {
+        searched.value = value;
+      } else {
+        table[index].add(new Node(key, value));
+      }
+    }
+  }
+
+  public String get(String key) {
+    Long hashCode = getHashCode(key);
+    int index = getIndex(hashCode);
+
+    Node searched = searchNode(index, key);
+
+    if (searched == null) {
+      return "";
+    } else {
+      return searched.value;
+    }
+  }
+
+}
---
+++ src/main/java/grace/datastructuresgroupstudy/week6/Node.java
@@ -0,0 +1,11 @@
+package grace.datastructuresgroupstudy.week6;
+
+public class Node {
+  String key;
+  String value;
+
+  public Node(String key, String value) {
+    this.key = key;
+    this.value = value;
+  }
+}
